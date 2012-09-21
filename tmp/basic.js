9f9f73e18d681893f2f4b4a1c5e0a632757b5128
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-09-21 15:03:12 -0500

    not tracking tmp

--- tmp/basic.js
+++
@@ -1,8 +0,0 @@
-define("test/fixtures/basic",['handlebars'], function(Handlebars){
- return Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers;
-  
-
-
-  return "<p>Lorem ipsum.</p>\n";})
-});
