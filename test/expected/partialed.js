6569c7b414360152ce552831a983637694c4ecec
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-09-26 11:48:45 -0500

    updated test expectations

--- test/expected/partialed.js
+++ test/expected/partialed.js
@@ -5,6 +5,6 @@ var template = Handlebars.template(function (Handlebars,depth0,helpers,partials,
 
 
   return "<p>Lorem ipsum.</p>\n";})
-Handlebars.registerPartial('test/fixtures/basic', template)
+Handlebars.registerPartial('test.fixtures.basic', template)
 return template
 });
