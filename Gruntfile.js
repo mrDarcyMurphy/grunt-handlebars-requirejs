2940e7e40812c14336d0143eb9a79ccdc2e32c15
Mike Frey <mike.frey@tstmedia.com>
2013-02-20 16:12:43 -0600

    updated to work with grunt 0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function(grunt) {
     },
 
     // Tests
-    mocha: {
+    simplemocha: {
       files: ['test/*.js']
     }
 
@@ -75,8 +75,8 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', 'clean handlebars_requirejs mocha');
+  grunt.registerTask('test', ['clean', 'handlebars_requirejs', 'simplemocha']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', 'lint test');
+  grunt.registerTask('default', ['lint', 'test']);
 };
